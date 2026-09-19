f50bf35d606b851a346567245432f1fc47af501b
vansh-sindhwani <Vansh.35316@stu.upes.ac.in>
2026-09-19 12:42:47 +0530

    Add files via upload

---
+++ program1_day1.c
@@ -0,0 +1,21 @@
+//Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+
+int main ()
+{
+	int a,b,sum;
+	printf("ENTER FIRST NUMBER:");
+	scanf("%d",&a);
+	printf("ENTER SECOND NUMBER:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("SUM IS:%d",sum);
+	return 0;
+}
+/*
+C:\Users\exipc0202\OneDrive\Desktop\cprog>program1_day1.exe
+ENTER FIRST NUMBER:3
+ENTER SECOND NUMBER:4
+SUM IS:7
+*/
